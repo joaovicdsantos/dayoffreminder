5e025446f6625a62423ce82daf1ab5bb51e19ed9
João Victor <joaovicdsantos@gmail.com>
2022-06-15 22:43:58 -0300

    test

--- handler/dayoff.go
+++ handler/dayoff.go
@@ -20,6 +20,7 @@ func CreateDayOff(c *fiber.Ctx) error {
 	db := database.DBConn
 
 	log.Print(c.Request().URI().QueryString())
+	log.Print(c.Body())
 	if err := dayOff.QueryToDayOff(c.Query("text"), c.Query("user_name")); err != nil {
 		log.Print(err.Error())
 		c.JSON(fiber.Map{
